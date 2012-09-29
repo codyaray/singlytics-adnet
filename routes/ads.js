417ba2cff59b5f0cae91266cc5e22d534c3e242e
Benj. N. Sugar <git@skilfullycurled.org>
2012-09-29 15:01:07 -0500

    added products if/else chain to adsroute

--- routes/ads.js
+++ routes/ads.js
@@ -1,3 +1,36 @@
 exports.ads = function(req, res){
-  res.send('{"image" : "http://upload.wikimedia.org/wikipedia/commons/2/24/Ad-MediumRectangle-300x250.jpg","href":"http://www.singly.com"}');
+  
+  if (req.param('category')) {
+  	
+  	var category = req.param('category');
+  
+  	if(category == 'fashion'){
+  		
+  		res.send('{"category":"fashion", "image" : "http://upload.wikimedia.org/wikipedia/commons/2/24/Ad-fashion-300x250.jpg","href":"http://www.singly.com"}');
+  	
+  	} else if (category == 'motors') {
+  		
+  		res.send('{"category":"motors", "image" : "http://upload.wikimedia.org/wikipedia/commons/2/24/Ad-motors-300x250.jpg","href":"http://www.singly.com"}');
+  	
+  	} else if (category == 'home') {
+  		
+  		res.send('{"category":"home", "image" : "http://upload.wikimedia.org/wikipedia/commons/2/24/Ad-home-300x250.jpg","href":"http://www.singly.com"}');
+  
+  	} else if (category == 'electronics') {
+  		
+  		res.send('{"category":"electronics", "image" : "http://upload.wikimedia.org/wikipedia/commons/2/24/Ad-electronics-300x250.jpg","href":"http://www.singly.com"}');
+  	
+  	} else if (category == 'food') {
+  		
+  		res.send('{"category":"food", "image" : "http://upload.wikimedia.org/wikipedia/commons/2/24/Ad-food-300x250.jpg","href":"http://www.singly.com"}');
+  	
+  	} else if (category == 'entertainment') {
+  		
+  		res.send('{"category":"entertainment", "image" : "http://upload.wikimedia.org/wikipedia/commons/2/24/Ad-entertainment-300x250.jpg","href":"http://www.singly.com"}');
+  	
+  	} else if (category == 'sports') {
+  		
+  		res.send('{"category":"sports", "image" : "http://upload.wikimedia.org/wikipedia/commons/2/24/Ad-sports-300x250.jpg","href":"http://www.singly.com"}');
+  	};
+  };
 };
