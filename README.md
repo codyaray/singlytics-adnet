1316cc8d88dbbb6424e7632176aff30b8a8d3c85
Leif Kramer <lkramer@brighttag.com>
2012-09-29 14:08:59 -0500

    Initial commit of adnet stub

---
+++ README.md
@@ -0,0 +1,3 @@
+# AdNet
+
+A mock adnet for testing Singlytics
---
+++ routes/index.js
@@ -0,0 +1,8 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.send('{"image" : "http://upload.wikimedia.org/wikipedia/commons/2/24/Ad-MediumRectangle-300x250.jpg","URL":"http://www.singly.com"}');
+};
