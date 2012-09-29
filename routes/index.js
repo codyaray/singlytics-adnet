3a831bc5fa43a5108bb8d24115bf6957f9183d2b
Leif Kramer <lkramer@brighttag.com>
2012-09-29 15:15:47 -0500

    udpate files

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,8 @@
  */
 
 exports.index = function(req, res){
-  res.send('{"image" : "http://upload.wikimedia.org/wikipedia/commons/2/24/Ad-MediumRectangle-300x250.jpg","URL":"http://www.singly.com"}');
+  res.send('{
+           "image" : "http://upload.wikimedia.org/wikipedia/commons/2/24/Ad-MediumRectangle-300x250.jpg",
+           "URL":"http://www.singly.com"
+           }');
 };
