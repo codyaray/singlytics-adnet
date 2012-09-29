998e734764e9e39be126225acebd396133b6b2b6
Leif Kramer <lkramer@brighttag.com>
2012-09-29 15:37:42 -0500

    fixed index

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,5 @@
  */
 
 exports.index = function(req, res){
-  res.send('{
-           "image" : "http://upload.wikimedia.org/wikipedia/commons/2/24/Ad-MediumRectangle-300x250.jpg",
-           "URL":"http://www.singly.com"
-           }');
+  res.send('{"image" : "http://upload.wikimedia.org/wikipedia/commons/2/24/Ad-MediumRectangle-300x250.jpg","URL":"http://www.singly.com"}');
 };
