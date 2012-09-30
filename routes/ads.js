1d3931ea31451cb869803a830bcd2f6498b46230
Cody A. Ray <sites@codyaray.com>
2012-09-30 12:25:30 -0500

    Support keywords - travel/mexico

--- routes/ads.js
+++ routes/ads.js
@@ -4,24 +4,34 @@ var _ = require('underscore'),
 var AD_DB = [
   {
     "id": 0,
+    "keywords": {},
     "weights": { "locale": { "en_US": 6, "en_GB": 4}, "gender": { "male": 3, "female": 8 }, "language": {}, "timezone": {}, "location": {} },
     "data": {"category":"dating", "image" : "http://farm9.staticflickr.com/8316/8037143421_eb3cbd0564.jpg","href":"http://www.singly.com"}
   },
   {
     "id": 1,
+    "keywords": {},
     "weights": { "locale": { "en_US": 3, "en_GB": 4}, "gender": { "male": 2, "female": 8 }, "language": {}, "timezone": {}, "location": {} },
     "data": {"category":"animals", "image" : "http://farm9.staticflickr.com/8454/8037143501_127043003d.jpg","href":"http://www.singly.com"},
   },
   {
     "id": 2,
+    "keywords": {},
     "weights": { "locale": { "en_US": 10 }, "gender": { "male": 8, "female": 2 }, "language": {}, "timezone": {}, "location": {} },
     "data": {"category":"football", "image" : "http://pagead2.googlesyndication.com/pagead/imgad?id=CICAgMDO89iH_AEQrAIY-gEyCCCUZbQUszgq","href":"http://www.nfl.com"}
   },
   {
     "id": 3,
+    "keywords": { "travel": 10, "mexico": 20 },
+    "weights": { "locale": {}, "gender": {}, "language": {}, "timezone": {}, "location": {} },
+    "data": {"category":"travel", "image" : "http://farm9.staticflickr.com/8031/8039955741_eefefaf910.jpg","href":"http://www.lonelyplanet.com/mexico"}
+  },
+  {
+    "id": 4,
+    "keywords": {},
     "weights": { "locale": { "en_US": 2, "en_GB": 4}, "gender": { "male": 4, "female": 2 }, "language": {}, "timezone": {}, "location": {} },
     "data": {"category":"golf", "image" : "http://farm9.staticflickr.com/8316/8037143535_9799b715c4.jpg","href":"http://www.singly.com"}
-  },
+  }
 ]
 
 exports.ads = function(req, res) {
@@ -31,6 +41,7 @@ exports.ads = function(req, res) {
   var gender = req.query.gender;
   var language = req.query.language;
   var interest = req.query.interest;
+  var keywords = (req.query.keywords || "").split(",");
   
   var scores = {};
   _.each(AD_DB, function(ad) {
@@ -51,6 +62,13 @@ exports.ads = function(req, res) {
         scores[ad.id].score += ad.weights.gender["female"] || 0;
         break;
     }
+    _.each(keywords, function(keyword) {
+      _.each(ad.keywords, function(value, key, list) {
+        if (key == keyword) {
+          scores[ad.id].score += ad.keywords[keyword];          
+        }
+      });
+    });
   });
 
   util.log("Scores:\n" + util.inspect(scores));
